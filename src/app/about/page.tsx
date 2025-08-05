1fe9bffff644045dab61fd0a7901005fc483fe93
Anaelica <anaanaelica@gmail.com>
2025-08-04 21:58:16 -0300

    feat: galeria de cards

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,44 @@
+// app/page.tsx ou qualquer página que importe Card
 import Card from "@/components/Card";
+
+const items = [
+  {
+    title: "Revisão de Componentes",
+    description: "Revisão das aulas de React e Next.js",
+    imageUrl: "https://i.pinimg.com/1200x/9b/4e/e0/9b4ee057076232fb57c48cf80947f8a7.jpg",
+    category: "Estudo",
+  },
+  {
+    title: "Fall Gays",
+    description: "Dispute com colegas em pistas desajeitadas e absurdas.",
+    imageUrl: "https://i.pinimg.com/1200x/56/c4/8a/56c48a8d444890c076f74d043cae3c6a.jpg",
+    category: "Jogo",
+  },
+  {
+    title: "One Piece",
+    description: "Luffy busca o tesouro lendário para se tornar o rei dos piratas.",
+    imageUrl: "/baixados (3).jpg",
+    category: "Anime",
+  },
+  {
+    title: "Vingadores: Guerra Infinita",
+    description: "É um filme de super-herói americano de 2018, baseado na equipe Vingadores da Marvel Comics",
+    imageUrl: "https://i.pinimg.com/1200x/7d/c6/85/7dc685ee97872f4e5ed23d9e6503434b.jpg",
+    category: "Filme",
+  },
+  {
+    title: "Fortnite",
+    description: "Reviva os primórdios do Battle Royale no Capítulo 1: Temporada 4. Mergulhe no passado e explore o mapa Raiz.",
+    imageUrl: "https://i.pinimg.com/736x/44/c6/01/44c60130de437ec9b361d61b5f86e7a4.jpg",
+    category: "Jogo",
+  },
+  {
+    title: "Dandadan",
+    description: "Uma missão espacial para salvar a humanidade.",
+    imageUrl: "/Turbo Granny - Ken - Momo.jpg",
+    category: "Anime",
+  },
+];
 
 export default function Home() {
   return (
@@ -10,13 +50,16 @@ export default function Home() {
         </p>
       </header>
 
-      <main className="flex items-center gap-7 justify-center px-4 py-10">
-        <Card
-          title="Revisão de Componentes"
-          description="Revisão das aulas de React e Next.js"
-          imageUrl="https://i.pinimg.com/1200x/9b/4e/e0/9b4ee057076232fb57c48cf80947f8a7.jpg"
-        />
-
+      <main className="grid lg:grid-cols-3 gap-8 px-20 py-12 justify-center">
+        {items.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            description={item.description}
+            imageUrl={item.imageUrl}
+            category={item.category}
+          />
+        ))}
       </main>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,15 +5,21 @@ type CardProps = {
   title: string;
   description: string;
   imageUrl: string;
+  category?: string; // opcional
 };
 
-export default function Card({ title, description, imageUrl }: CardProps) {
+export default function Card({ title, description, imageUrl, category }: CardProps) {
   return (
-    <div className="bg-white rounded-2xl shadow-lg overflow-hidden w-[300px] max-w-sm  transform transition-all duration-300 hover:scale-105 hover:shadow-2xl">
-      <img src={imageUrl} alt={title} className="w-full h-55 object-cover" />
+    <div className="bg-white rounded-2xl shadow-lg overflow-hidden w-[400px] h-[380px] max-w-sm transform transition-all duration-300 hover:scale-105 hover:shadow-2xl">
+      <img src={imageUrl} alt={title} className="w-full h-52 object-cover" />
       <div className="p-4">
-        <h2 className="text-blue-900 font-semibold mb-2">{title}</h2>
-        <p className="text-gray-900 font-semibold">{description}</p>
+        {category && (
+          <span className="inline-block bg-blue-200 text-blue-800 text-xs px-2 py-1 rounded-full uppercase font-bold mb-2">
+            {category}
+          </span>
+        )}
+        <h2 className="text-blue-900 font-semibold text-lg mb-2">{title}</h2>
+        <p className="text-gray-800 text-sm">{description}</p>
       </div>
     </div>
   );
